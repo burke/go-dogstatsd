dad8603d26401209ed15a16ca28b0a24d3ebb94c
Burke Libbey <burke@libbey.me>
2014-11-14 11:18:07 -0500

    return an erorr if the client is unconfigured

--- dog/dog.go
+++ dog/dog.go
@@ -1,6 +1,9 @@
 package dog
 
-import "github.com/Shopify/go-dogstatsd"
+import (
+	"errors"
+	"github.com/Shopify/go-dogstatsd"
+)
 
 var Client *dogstatsd.Client
 
@@ -14,21 +17,46 @@ func Configure(addr string, namespace string, tags []string) (err error) {
 	return
 }
 
+var ErrUnconfigured = errors.New("connection has not been configured; call dog.Configure()")
+
 func Event(title string, text string, tags []string) error {
+	if Client == nil {
+		return ErrUnconfigured
+	}
 	return Client.Event(title, text, tags)
 }
+
 func Gauge(name string, value float64, tags []string, rate float64) error {
+	if Client == nil {
+		return ErrUnconfigured
+	}
 	return Client.Gauge(name, value, tags, rate)
 }
+
 func Count(name string, value int64, tags []string, rate float64) error {
+	if Client == nil {
+		return ErrUnconfigured
+	}
 	return Client.Count(name, value, tags, rate)
 }
+
 func Histogram(name string, value float64, tags []string, rate float64) error {
+	if Client == nil {
+		return ErrUnconfigured
+	}
 	return Client.Histogram(name, value, tags, rate)
 }
+
 func Timer(name string, value float64, tags []string, rate float64) error {
+	if Client == nil {
+		return ErrUnconfigured
+	}
 	return Client.Timer(name, value, tags, rate)
 }
+
 func Set(name string, value string, tags []string, rate float64) error {
+	if Client == nil {
+		return ErrUnconfigured
+	}
 	return Client.Set(name, value, tags, rate)
 }
